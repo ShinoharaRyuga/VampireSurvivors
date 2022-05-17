51538f649476d641e14f07762cb4ca772bf05f57
SinoharaRyuga <raitan3170@gmail.com>
2022-05-17 23:56:25 +0900

    プレイヤーの動きを作成

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField, Tooltip("ïêäÌéÌ")] WeaponBase[] _weapons = default;
+    [SerializeField, Tooltip("à⁄ìÆë¨ìx")] int _moveSpeed = 0;
+    Rigidbody2D _rb2D = default;
+
+    void Start()
+    {
+        _rb2D = GetComponent<Rigidbody2D>();
+        StartCoroutine(_weapons[0].Generator(transform.up));
+    }
+
+    void Update()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+        Vector2 dir = new Vector2(h, v) * _moveSpeed;
+        if (dir != Vector2.zero)
+        {
+            transform.up = dir;
+            _rb2D.velocity = dir;
+        }
+    }
+}
